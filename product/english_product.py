07818706d66f09b01ca3dbb7f2791e38f0dd7a81
Byron Mandela <mandelabyron@gmail.com>
2023-06-16 01:08:09 +0300

    added cmd args

--- product/english_product.py
+++ product/english_product.py
@@ -1,11 +1,15 @@
 from product.spiders.english import EnglishSpider
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
+from sys import argv
 
+
+url=argv[1]
+#"https://www.englishhome.com/absy-zen-dokuma-hali-160x230-cm-lacivert/"
 def main(): 
     settiings=get_project_settings()
     process = CrawlerProcess(settings=settiings)
-    process.crawl(EnglishSpider,product_url="https://www.englishhome.com/absy-zen-dokuma-hali-160x230-cm-lacivert/")
+    process.crawl(EnglishSpider,product_url=url)
     process.start()
     
 if __name__ == '__main__':
